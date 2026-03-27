bb4178bab630ed1ee958e6f63b4b07974acc458b
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-27 23:59:26 +0600

    Display Categories

--- script/index.js
+++ script/index.js
@@ -7,4 +7,12 @@ function loadCategories () {
 loadCategories ();
 
 function displayCategories(categories) {
-  console.log(categories);
+  const categoryContainer = document.getElementById("category-container");
+  for (let cat of categories) {
+    const categoryDiv = document.createElement("div");
+ q  categoryDiv.innerHTML = `
+    <button class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
+    `;
+    categoryContainer.append(categoryDiv);
+  }
+}
