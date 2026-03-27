bee6830343b2fd66820edb97be61de515842ab31
fatematujjohra <fatematujjohralima2@gmail.com>
2026-03-28 00:17:03 +0600

    video card running

--- script/index.js
+++ script/index.js
@@ -1,18 +1,30 @@
+
 function loadCategories () {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
     .then((res)=>res.json())
     .then((data)=>displayCategories(data.categories));
 
 }
-loadCategories ();
+
+function loadVideos(video) {
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos`
+  )
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
+}
+
 
 function displayCategories(categories) {
   const categoryContainer = document.getElementById("category-container");
   for (let cat of categories) {
     const categoryDiv = document.createElement("div");
- q  categoryDiv.innerHTML = `
+    categoryDiv.innerHTML = `
     <button class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
     `;
     categoryContainer.append(categoryDiv);
   }
 }
+
+loadCategories ();
+loadVideos();
